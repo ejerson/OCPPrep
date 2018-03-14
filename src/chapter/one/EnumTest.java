7a4b10daca88d2c8637c5c8ccb25b8b6fcd818e5
ejerson <ejersonb@gmail.com>
2018-03-14 11:46:06 -0600

    enums explored

--- src/chapter/one/EnumTest.java
+++ src/chapter/one/EnumTest.java
@@ -5,9 +5,9 @@ public class EnumTest {
   public static void main(String[] args) {
     Season e = Season.FALL;
     // prints the name of enum
-    System.out.println(e.name());
+    System.out.println(e.name()); // FALL
     // prints the order in which it is declared
-    System.out.println(e.ordinal());
+    System.out.println(e.ordinal()); // 3
 
     System.out.println(e.compareTo(Season.SUMMER));
 
