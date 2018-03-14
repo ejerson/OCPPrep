21d23bad8362f934c930026808d267a9fe65d18e
ejerson <ejersonb@gmail.com>
2018-03-14 10:37:50 -0600

    equals() method added

--- src/chapter/one/Equals.java
+++ src/chapter/one/Equals.java
@@ -16,5 +16,22 @@ public class Equals {
     // false => uses .equals() implementation
     // from Object, which simply checks if
     // two objects references being compared points to the same object
+
+
   }
+
+
+  @Override
+  public boolean equals(Object obj) {
+    // checks to see if an object is of the same class
+    if (!(obj instanceof Equals)) return false;
+    return true;
+  }
+
+  // Use Apache Commons Lang to make overriding equals more convenient
+//  @Override
+//  public boolean equals(Object obj) {
+//    // to check all instance variables
+//    return EqualsBuilder.reflectionEquals(this, obj);
+//  }
 }
